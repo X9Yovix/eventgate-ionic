9dc8b8b0c6225f6a0d01c18233ce82ae28da00a3
Karim Ouazzou <karimouazzou@gmail.com>
2024-10-03 16:12:44 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eventgate_ionic',
+  webDir: 'www'
+};
+
+export default config;
